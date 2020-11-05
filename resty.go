372b851489b64a23910a509976a2f0a05d142a35
storezhang <storezhang@gmail.com>
2020-11-05 15:42:38 +0800

    YUNKE-1781 【Arch】支持Open API鉴权

--- resty.go
+++ resty.go
@@ -6,14 +6,6 @@ import (
 	`github.com/go-resty/resty/v2`
 )
 
-type (
-	response struct {
-		ErrorCode int         `json:"errorCode"`
-		Message   string      `json:"message"`
-		Data      interface{} `json:"data"`
-	}
-)
-
 // NewResty Resty客户端
 func NewResty() *resty.Request {
 	return resty.New().SetTLSClientConfig(&tls.Config{InsecureSkipVerify: true}).R()
